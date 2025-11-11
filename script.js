c66e0187db7e79050bac5885584c0a7ea406a741
saholiv8 <saholiv8@gmail.com>
2025-11-11 20:45:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 const produtos = {
   vendidos: [
-    { nome: "Jaqueta Puff", preco: "R$ 169,90" },
-    { nome: "Vestido Indiano", preco: "R$ 119,90" },
-    { nome: "Calça Cargo", preco: "R$ 119,90" }
+    {
+      nome: "Jaqueta Puff",
+      preco: "R$ 169,90",
+      imagem: "img/jaqueta.jpg"
+    },
+    {
+      nome: "Vestido Indiano",
+      preco: "R$ 119,90",
+      imagem: "img/indiano.jpg"
+    },
+    {
+      nome: "Calça Cargo",
+      preco: "R$ 119,90",
+      imagem: "img/cargo.jpg"
+    }
   ],
   baratos: [
-    { nome: "Short Jeans", preco: "R$ 79,00" },
-    { nome: "Calça Jeans Skin", preco: "R$ 99,90" },
-    { nome: "Cropped", preco: "R$ 49,90" }
+    {
+      nome: "Short Jeans",
+      preco: "R$ 79,00",
+      imagem: "img/short.jpg"
+    },
+    {
+      nome: "Calça Jeans Skin",
+      preco: "R$ 99,90",
+      imagem: "img/skin.jpg"
+    },
+    {
+      nome: "Cropped",
+      preco: "R$ 49,90",
+      imagem: "img/cropped.jpg"
+    }
   ],
-  estilosos: [
-    { nome: "Vestido Florido", preco: "R$ 159,90" },
-    { nome: "Jaqueta de Couro", preco: "R$ 169,90" },
-    { nome: "Blusa Polo", preco: "R$ 159,90" }
-  ]
-};
-
-function mostrarAba(aba) {
-  const container = document.getElementById("catalogo");
-  container.innerHTML = "";
-
-  produtos[aba].forEach(produto => {
-    const div = document.createElement("div");
-    div.className = "produto";
-    div.innerHTML = `<h3>${produto.nome}</h3><p>${produto.preco}</p>`;
-    container.appendChild(div);
-  });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  mostrarAba("vendidos");
-});
