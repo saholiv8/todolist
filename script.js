d8522776e8c49c2ce5f5d0478f1685428bd4cc48
saholiv8 <saholiv8@gmail.com>
2025-11-11 20:07:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const produtos = [
+  { nome: "Camiseta", preco: "R$ 49,90" },
+  { nome: "Calça Jeans", preco: "R$ 89,90" },
+  { nome: "Vestido", preco: "R$ 129,90" }
+];
+
+const container = document.getElementById("produtos");
+
+produtos.forEach(produto => {
+  const div = document.createElement("div");
+  div.className = "produto";
+  div.innerHTML = `<h3>${produto.nome}</h3><p>${produto.preco}</p>`;
+  container.appendChild(div);
+});
 
