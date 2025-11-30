60f86aee1f6abe1574327e04a5ed3a3bdfdd4f78
sara santos <saholiv8@gmail.com>
2025-11-30 19:34:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-const produtos = {
-  vendidos: [
-    {
-      nome: "Jaqueta Puff",
-      preco: "R$ 169,90",
-      imagem: "img/jaqueta.jpg"
-    },
-    {
-      nome: "Vestido Indiano",
-      preco: "R$ 119,90",
-      imagem: "img/indiano.jpg"
-    },
-    {
-      nome: "Calça Cargo",
-      preco: "R$ 119,90",
-      imagem: "img/cargo.jpg"
-    }
-  ],
-  baratos: [
-    {
-      nome: "Short Jeans",
-      preco: "R$ 79,00",
-      imagem: "img/short.jpg"
-    },
-    {
-      nome: "Calça Jeans Skin",
-      preco: "R$ 99,90",
-      imagem: "img/skin.jpg"
-    },
-    {
-      nome: "Cropped",
-      preco: "R$ 49,90",
-      imagem: "img/cropped.jpg"
-    }
-  ],
+function adicionarTarefa() {
+  const input = document.getElementById("tarefa");
+  const texto = input.value.trim();
+
+  if (texto !== "") {
+    const li = document.createElement("li");
+    li.textContent = texto;
+
+    // remover tarefa ao clicar
+    li.onclick = () => li.remove();
+
+    document.getElementById("lista").appendChild(li);
+    input.value = "";
+  }
+}
